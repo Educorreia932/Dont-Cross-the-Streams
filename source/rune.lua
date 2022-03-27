00d59182a784848c176ec0488dc6e3e31a2d31cb
BarbaraDP <bdianapinto@outlook.com>
2022-03-27 13:40:46 +0100

    Fixed rune GUI.

--- source/rune.lua
+++ source/rune.lua
@@ -5,6 +5,8 @@ local gfx <const> = pd.graphics
 Rune = {}
 Rune.__index = Rune
 
+local displayedRune = {}
+
 function Rune:new(x, y, i, portal)
     local rune = {}
     setmetatable(rune, Rune)
@@ -23,18 +25,21 @@ function Rune:new(x, y, i, portal)
     return rune
 end
 
-function displayRuneGUI(rune)
+function displayRuneGUI()
     local ui = gfx.image.new("images/UI_slot")
     ui_sprite = gfx.sprite.new(ui)
     ui_sprite:setIgnoresDrawOffset(true)
     ui_sprite:moveTo(gui.left_align.x, gui.left_align.y)
     ui_sprite:add()
 
-    if rune ~= nil then
-        local image = gfx.image.new("images/big_rune".. rune.i)
-        rune.sprite = gfx.sprite.new(image)
-        rune.sprite:setIgnoresDrawOffset(true)
-        rune.sprite:moveTo(gui.left_align.x, gui.left_align.y)
-        rune.sprite:add()
+    if player.holding_rune ~= nil then
+        local image = gfx.image.new("images/big_rune".. player.holding_rune.i)
+        
+        if displayedRune.sprite ~= nil then displayedRune.sprite:remove() end
+        
+        displayedRune.sprite = gfx.sprite.new(image)
+        displayedRune.sprite:setIgnoresDrawOffset(true)
+        displayedRune.sprite:moveTo(gui.left_align.x, gui.left_align.y)
+        displayedRune.sprite:add()
     end
 end
