ea7a44586c7895e9c93b64a85729be8a2f1e81c7
Educorreia932 <skelozard1@gmail.com>
2022-03-27 15:32:24 +0100

    Playing sound effect when picking up a rune

--- source/music.lua
+++ source/music.lua
@@ -1,6 +1,17 @@
 local player
 
+local enter_portal_player = playdate.sound.fileplayer.new("sounds/portal")
+local loop_music_player = playdate.sound.fileplayer.new("sounds/spooky")
+local grab_rune_player = playdate.sound.fileplayer.new("sounds/grabRune")
+
 function loopGameMusic()
-    player = playdate.sound.fileplayer.new("sounds/spooky")
-    player.play(player, 0)
+    loop_music_player:play(0)
+end
+
+function playPortalSound()
+    enter_portal_player:play(1)
+end
+
+function playGrabRuneSound()
+    grab_rune_player:play(1)
 end
